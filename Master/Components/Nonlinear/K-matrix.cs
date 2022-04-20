bb771dfeed9d6d6d1bc51392f4f743764e261511
Nanna Thoen Frogner <nannatf@ntnu.no>
2022-04-20 14:45:17 +0200

    startingwith NonLin

---
+++ Master/Components/Nonlinear/K-matrix.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double;
+using System.Linq;
+using MathNet.Numerics.LinearAlgebra.Factorization;
+using Grasshopper.Kernel.Data;
+using Grasshopper.Kernel.Types;
+using MathNet.Numerics.Integration;
+
+namespace Master.Components.Nonlinear
+{
+    public class K_matrix : GH_Component
+    {
+        /// <summary>
+        /// Initializes a new instance of the K_matrix class.
+        /// </summary>
+        public K_matrix()
+          : base("K-matrix", "Nickname",
+              "Description",
+              "Panda", "3DBeam")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddNumberParameter("Degree","deg","Degree of nonlinearity",GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddGenericParameter("MatrixClass", "mC", "Matrix class with degree and element matrix", GH_ParamAccess.item);
+           
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            int deg = 1;
+
+            DA.GetData(0, ref deg);
+
+            for (int i = 0; i < (deg+1) *2; i++)
+            {
+
+            }
+
+
+        }
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                //You can add image files to your project resources and access them like this:
+                // return Resources.IconForThisComponent;
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("7A182C6B-AFC8-4803-A860-635DDAEAD98D"); }
+        }
+    }
+}
