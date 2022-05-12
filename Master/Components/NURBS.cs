c623396b8cd27b60ac2f7f6bbb2f058f58856da6
Marte Møkkelgård <ma3489@gmail.com>
2022-05-12 15:12:38 +0200

    nurbs, riktig tall på ke for aksial

--- Master/Components/NURBS.cs
+++ Master/Components/NURBS.cs
@@ -52,62 +52,76 @@ namespace Master.Components
         {
 
             List<Point3d> ipts = new List<Point3d>();
-            int ngp = new int();
+            double ngp = new double();
             List<double> X = new List<double>();
-            int p = new int();
+            double pp = new double();
 
 
 
             DA.GetDataList(0, ipts);
             DA.GetData(1, ref ngp);
             DA.GetDataList(2, X);
-            DA.GetData(3, ref p);
+            DA.GetData(3, ref pp);
 
+            ngp = Convert.ToInt32(ngp);
+            int p = Convert.ToInt32(pp);
 
             List<double> Xi = new List<double>();
-            for (int i=0; i<ngp; i++)
+            List<double> GPW = new List<double>();
+            if (ngp == 2)
             {
-                Xi.Add(i / (ngp - 1));
+                Xi.Add(-Math.Sqrt(1.0 / 3.0));
+                Xi.Add(Math.Sqrt(1.0 / 3.0));
+                GPW.Add(1.0);
+                GPW.Add(1.0);
             }
 
-            foreach(double xi in Xi)
+            //control points
+
+            var cpts = ipts;
+
+            // degree
+            int pu = 1;
+            int pv = p;
+            int cpc = cpts.Count;
+
+            int dim = pu + 1 + pv + 1;
+            Matrix<double> k_e = DenseMatrix.OfArray(new double[dim, dim]);
+
+            for (int n = 0; n < Xi.Count; n++)
             {
                 //info
                 var info = new List<string>() { "code for NURBS" };
 
-                //control points
-
-                var cpts = ipts;
-
-
+                
                 //knot vector
-                List<double> kV = X;// new List<double>(){0,0,0,0.5,1,1,1};
+                List<double> kVv = X;// new List<double>(){0,0,0,0.5,1,1,1};
+                List<double> kVu = new List<double>(){-1,-1,1,1};
 
                 string kVstring = "KnotVec = ";
-                foreach (var v in kV)
+                foreach (var v in kVv)
                     kVstring = kVstring + " " + v;
                 info.Add(kVstring);
 
-                //degree
-                int pu = 1;
-                int pv = p;
-                int n = cpts.Count;
-
+              
                 //double xi = 0.49;
 
                 //basic shape function degree 0
-                List<double> N0 = getN0(kV, xi);
+                List<double> N0v = getN0(kVv, Xi[n]);
+                List<double> N0u = getN0(kVu, Xi[n]);
                 string N0string = "N0 = ";
-                foreach (var n0 in N0)
+                foreach (var n0 in N0v)
                     N0string = N0string + " " + n0;
                 info.Add(N0string);
 
                 //basic shape function degree 1 and higher
                 int d = 1;
-                List<double> N = getN(kV, xi, p);
-                List<double> dN = getdN(kV, xi, p, d);
+                List<double> Nu = getN(kVu, Xi[n], pu);
+                List<double> dNu = getdN(kVu, Xi[n], pu, d);
+                List<double> Nv = getN(kVv, Xi[n], pv);
+                List<double> dNv = getdN(kVv, Xi[n], pv, d);
                 string Nstring = "N = ";
-                foreach (var nshp in N)
+                foreach (var nshp in Nv)
                     Nstring = Nstring + " " + nshp;
                 info.Add(Nstring);
 
@@ -116,39 +130,51 @@ namespace Master.Components
                 double J = L / 2;
                 double A = 0.2;
                 double I = 0.014;
-                double E = 21000;
+                double E = 1525;
 
 
                 //Selective basis functions (for vertical disp and rot)
 
                 int db = 2;   //derived d-times
-                double B0 = getdN(kV, xi, p, d)[0] + getdN(kV, xi, p, db)[1];
-                double B1 = getdN(kV, xi, p, d)[2] + getdN(kV, xi, p, db)[3];
-                double B2 = L / 3 * getdN(kV, xi, p, db)[1];
-                double B3 = -L / 3 * getdN(kV, xi, p, db)[2];
+                List<double> B = new List<double>();
+                double B0 = getdN(kVv, Xi[n], pv, db)[0] + getdN(kVv, Xi[n], pv, db)[1];
+                double B1 = getdN(kVv, Xi[n], pv, db)[2] + getdN(kVv, Xi[n], pv, db)[3];
+                double B2 = L / 3 * getdN(kVv, Xi[n], pv, db)[1];
+                double B3 = -L / 3 * getdN(kVv, Xi[n], pv, db)[2];
+                B.Add(B0);B.Add(B1);B.Add(B2);B.Add(B3);
 
 
                 //Making K-matrix
 
 
-                List<double> GPW = new List<double>() { 2, 1, }; 
-                int dim = pu + 1 + pv + 1;
-                Matrix<double> k_e = DenseMatrix.OfArray(new double[dim, dim]);
+                
 
-                for (int a = 0; a < N.Count; a++)
+                for (int a = 0; a < Nu.Count; a++)
                 {
-                    for (int b = 0; b < N.Count; b++)
+                    for (int b = 0; b < Nu.Count; b++)
                     {
-                        k_e.Column(a)[b] += (E * A / J * J) * dN[a] * dN[b] * J * GPW[ngp - 1];
+                        k_e[3*a,3*b] += (E * A / (J * J)) * dNu[a] * dNu[b] * J * GPW[n];
                     }
                 }
+
+                
+                for (int g = 0; g < Nv.Count/2; g++)
+                {
+                    for (int e = 0; e < Nv.Count/2; e++)
+                    {
+                        k_e[g+1,e+1] += (E * I / (J * J*J*J)) * B[e] * B[g] * J * GPW[n];
+                        k_e[g+1,4+e] += (E * I / (J * J*J*J)) * B[e] * B[g+2] * J * GPW[n];
+                        k_e[4+g, e+1] += (E * I /( J * J * J * J)) * B[e+2] * B[g] * J * GPW[n];
+                        k_e[4+g, e+4] += (E * I / (J * J * J * J)) * B[e+2] * B[g+2] * J * GPW[n];
+                    }
+                }
+
                 
 
 
-
-    //calculate
-                int ispan = findSpan(kV, xi);
-                info.Add("ispan for xi " + xi + " is =" + ispan);
+                //calculate
+                int ispan = findSpan(kVv, Xi[n]);
+                info.Add("ispan for xi " + Xi[n] + " is =" + ispan);
 
                 double posX = 0;
                 double posY = 0;
@@ -156,9 +182,9 @@ namespace Master.Components
 
                 for (int c = 0; c < cpts.Count; c++)
                 {
-                    posX = posX + cpts[c].X * N[c];
-                    posY = posY + cpts[c].Y * N[c];
-                    posZ = posZ + cpts[c].Z * N[c];
+                    posX = posX + cpts[c].X * Nv[c];
+                    posY = posY + cpts[c].Y * Nv[c];
+                    posZ = posZ + cpts[c].Z * Nv[c];
                 }
 
                 Point3d pos = new Point3d(posX, posY, posZ);
@@ -167,10 +193,12 @@ namespace Master.Components
                 info.Add("Zcord = " + posX);
 
 
-                infos = info;
-                opts = cpts;
-                opt = pos;
+               
             }
+
+           
+
+            DA.SetData(0, k_e);
         }
 
             
@@ -255,8 +283,9 @@ namespace Master.Components
             List<double> dN = new List<double>();
 
             List<double> N0 = getN0(kV, xi); //get basic shape function
+            
 
-            if (d == 1)
+            if (d == 1 && p ==1)
             {
                 for (int i = 0; i < kV.Count - p - 1; i++)
                 {
@@ -272,12 +301,34 @@ namespace Master.Components
                     if (b2 != 0)
                         b = b1 / b2;
 
-                    double n = a * getN(kV, xi, p - 1)[i] + b * getN(kV, xi, p - 1)[i + 1];
+                    double n = a * N0[i] + b * N0[i + 1];
                     dN.Add(n);
 
                 }
             }
-            else if (p > 1)
+
+            else if (d == 1 && p > 1)
+            {
+                for (int i = 0; i < kV.Count - p - 1; i++)
+                {
+                    double a1 = p;
+                    double a2 = kV[i + p] - kV[i];
+                    double a = 0;
+                    if (a2 != 0)
+                        a = a1 / a2;
+
+                    double b1 = p;
+                    double b2 = kV[i + p + 1] - kV[i + 1];
+                    double b = 0;
+                    if (b2 != 0)
+                        b = b1 / b2;
+
+                    double n = a * getN(kV, xi, p-1)[i] + b * getN(kV, xi, p - 1)[i + 1];
+                    dN.Add(n);
+
+                }
+            }
+            else if (d > 1 && p > 1)
             {
                 for (int i = 0; i < kV.Count - p - 1; i++)
                 {
